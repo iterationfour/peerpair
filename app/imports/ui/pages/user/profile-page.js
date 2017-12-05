90a329976c06db0a53fa9b89930ea8649434f5a5
kiankoba <kiankoba@users.noreply.github.com>
2017-12-04 20:45:21 -1000

    Fixed Edit Profile Page to make it functional
    I broke it in a previous commit (oops) but it's fixed again now

--- app/imports/ui/pages/user/profile-page.js
+++ app/imports/ui/pages/user/profile-page.js
@@ -46,17 +46,17 @@ Template.Profile_Page.events({
     event.preventDefault();
     const firstName = event.target.First.value;
     const lastName = event.target.Last.value;
-    const title = event.target.Title.value;
+    //const title = event.target.Title.value;
     const username = FlowRouter.getParam('username'); // schema requires username.
-    const picture = event.target.Picture.value;
+    const picture = event.target['Profile Picture'].value;
     const github = event.target.Github.value;
     const facebook = event.target.Facebook.value;
     const instagram = event.target.Instagram.value;
-    const bio = event.target.Bio.value;
-    const selectedInterests = _.filter(event.target.Interests.selectedOptions, (option) => option.selected);
+    const bio = event.target["About Me"].value;
+    const selectedInterests = _.filter(event.target.Major.selectedOptions, (option) => option.selected);
     const interests = _.map(selectedInterests, (option) => option.value);
 
-    const updatedProfileData = { firstName, lastName, title, picture, github, facebook, instagram, bio, interests,
+    const updatedProfileData = { firstName, lastName, /*title,*/ picture, github, facebook, instagram, bio, interests,
       username };
 
     // Clear out any old validation errors.
