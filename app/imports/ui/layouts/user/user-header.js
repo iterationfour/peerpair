bdb1b08b648a6f30302d650aa332945c8e539754
jessiejames <jlblanke@hawaii.edu>
2017-12-12 03:59:27 -1000

    fixed the route options in user menu  bar

--- app/imports/ui/layouts/user/user-header.js
+++ app/imports/ui/layouts/user/user-header.js
@@ -1,8 +1,9 @@
 import { Template } from 'meteor/templating';
 import { FlowRouter } from 'meteor/kadira:flow-router';
+import { Meteor } from 'meteor/meteor';
 
 Template.User_Header.helpers({
   routeUserName() {
-    return FlowRouter.getParam('username');
+    return Meteor.user().profile.name;
   },
 });
